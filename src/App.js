0865e7cbb58a5f561801cdf95921508130bf621b
Meggan <meggdo@gmail.com>
2018-06-12 22:17:58 -0400

    getWeather func isn't broken anymore..

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "d4e11fd4bdbf1547b5d617f79d85484d";
+
 class App extends React.Component {
+  //getWeather func
+  getWeather = async (e) => {
+    //prevent full page load
+    e.preventDefault();
+    //using async await
+    const api_call = await fetch("http://api.openweathermap.org/data/2.5/weather?q=Montreal&mode=json&appid=" +API_KEY+ "&units=metric");
+    const data = await api_call.json();
+    console.log(data);
+  }
+
+  //render
   render(){
     return(
       //this is JSX. Babel converts this to JS..
       //Can only have one parent
       <div>
         <Titles/>
-        <Form/>
+        <Form getWeather={this.getWeather}/>
         <Weather/>
       </div>
   );

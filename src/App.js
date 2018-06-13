038af9cb8770c20ab13d769bfc8c8900281bbcff
Meggan <meggdo@gmail.com>
2018-06-13 14:54:24 -0400

    error handling

--- src/App.js
+++ src/App.js
@@ -26,39 +26,51 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     //using async await
     const api_call = await fetch("http://api.openweathermap.org/data/2.5/weather?q="
-        +city+","+country+"&appid=" +API_KEY+ "&units=metric");
+      + city + "," + country + "&appid=" + API_KEY + "&units=metric");
     const data = await api_call.json();
-    console.log(data);
 
-    //set state
+    if (city && country) {
+      console.log(data);
+
+      //set state
       this.setState({
-          temperature: data.main.temp,
-          city: data.name,
-          country: data.sys.country,
-          humidity: data.main.humidity,
-          description: data.weather[0].description,
-          error: ""
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
       });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter a value"
+      });
+    }
   }
 
   //render
-  render(){
-    return(
+  render() {
+    return (
       //this is JSX. Babel converts this to JS..
       //Can only have one parent
       <div>
-        <Titles/>
-        <Form getWeather={this.getWeather}/>
-        <Weather 
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather
           temperature={this.state.temperature}
-          city ={this.state.city}
+          city={this.state.city}
           country={this.state.country}
           humidity={this.state.humidity}
           description={this.state.description}
           error={this.state.error}
         />
       </div>
-  );
+    );
   }
 };
 

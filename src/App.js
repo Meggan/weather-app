94c11c5d3ce5a2e0487ae84b1bad75612c7ce8c2
Meggan <meggdo@gmail.com>
2018-06-13 14:46:09 -0400

    displays info if country && city are present

--- src/App.js
+++ src/App.js
@@ -7,14 +7,38 @@ import Weather from "./components/Weather";
 const API_KEY = "d4e11fd4bdbf1547b5d617f79d85484d";
 
 class App extends React.Component {
+  //creating a state
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
   //getWeather func
   getWeather = async (e) => {
     //prevent full page load
     e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
     //using async await
-    const api_call = await fetch("http://api.openweathermap.org/data/2.5/weather?q=Montreal&mode=json&appid=" +API_KEY+ "&units=metric");
+    const api_call = await fetch("http://api.openweathermap.org/data/2.5/weather?q="
+        +city+","+country+"&appid=" +API_KEY+ "&units=metric");
     const data = await api_call.json();
     console.log(data);
+
+    //set state
+      this.setState({
+          temperature: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          humidity: data.main.humidity,
+          description: data.weather[0].description,
+          error: ""
+      });
   }
 
   //render
@@ -25,7 +49,14 @@ class App extends React.Component {
       <div>
         <Titles/>
         <Form getWeather={this.getWeather}/>
-        <Weather/>
+        <Weather 
+          temperature={this.state.temperature}
+          city ={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
   );
   }
